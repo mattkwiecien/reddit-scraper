865686d82b99f99ca97c85ba365c71bd746d0408
Matthew Kwiecien <mkwieci2@gmail.com>
2019-01-13 10:21:27 -0600

    Adding a simple ASP.NET core mvc test site to scrape against.

---
+++ TestSite/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TestSite.Models;
+
+namespace TestSite.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index() {
+            var sd = new SampleData();
+            return View(sd);
+        }
+
+        public IActionResult ViewDetail(int id) {
+            SampleData sd = new SampleData();
+            sd.SetSelected(id);
+            return View(sd);
+        }
+
+        [HttpPost()]
+        [ActionName("FormData")]
+        public IActionResult FormDataPost() {
+            var fd = Request.Form;
+            ViewBag.Name = fd["UserName"];
+            ViewBag.Gender = fd["Gender"];
+            return View("~/Views/Home/PostSuccess.cshtml");
+        }
+
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TestSite/Models/SampleData.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestSite.Models {
+
+    public class SampleData {
+
+        public List<Country> Countries { get; set; }
+        public List<Person> People { get; set; }
+        public int SelectedID { get; set; }
+        public int SelectedCountryID { get; set; }
+        public string SelectedName { get; set; }
+
+        public SampleData() {
+            Countries = new List<Country>();
+            People = new List<Person>();
+
+            Countries.Add(new Country(1, "United Kingdom"));
+            Countries.Add(new Country(2, "United States"));
+            Countries.Add(new Country(3, "Republic of Ireland"));
+            Countries.Add(new Country(4, "India"));
+
+
+            People.Add(new Person(1, 1, "AJSON"));
+            People.Add(new Person(2, 2, "Fred"));
+            People.Add(new Person(3, 2, "Mary"));
+        }
+
+        public void SetSelected(int ID) {
+            var p = People.SingleOrDefault(x => x.ID == ID);
+            if (p == null) { return; }
+            this.SelectedID = p.ID;
+            this.SelectedCountryID = p.Nationality;
+            this.SelectedName = p.Name;
+        }
+
+    }
+
+    public class Person {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Nationality { get; set; }
+
+        public Person(int id, int nationality, string name) {
+            ID = id;
+            Nationality = nationality;
+            Name = name;
+        }
+    }
+
+    public class Country {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public Country(int id, string name) {
+            ID = id;
+            Name = name;
+        }
+    }
+}
